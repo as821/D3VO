c2e04e01e5a28b31c3ad4baef8c88508ad3b5eaf
Andrew Stange <andrew.r.stange@gmail.com>
2022-11-28 18:28:07 -0500

    move d3vo python binding into separate file

--- g2opy/python/types/sba/types_six_dof_expmap.h
+++ g2opy/python/types/sba/types_six_dof_expmap.h
@@ -2,7 +2,7 @@
 #include <pybind11/eigen.h>
 
 #include <g2o/types/sba/types_six_dof_expmap.h>
-#include <g2o/types/sba/types_d3vo.h>
+#include "types_d3vo.h"
 
 #include <g2o/types/slam3d/se3quat.h>
 //#include "python/core/base_vertex.h"
@@ -134,29 +134,6 @@ void declareTypesSixDofExpmap(py::module & m) {
     // class EdgeStereoSE3ProjectXYZ : public BaseBinaryEdge<3, Vector3D, VertexSBAPointXYZ, VertexSE3Expmap>
     // class EdgeStereoSE3ProjectXYZOnlyPose : public BaseUnaryEdge<3, Vector3D, VertexSE3Expmap>
 
-}
-
-
-void declareD3VO(py::module & m) {
-    py::class_<EdgeProjectD3VO, BaseMultiEdge<2, Vector2D>>(m, "EdgeProjectD3VO")
-        .def(py::init())
-        .def("compute_error", &EdgeProjectD3VO::computeError)
-        .def("linearize_oplus", &EdgeProjectD3VO::linearizeOplus)
-    ;
-
-    // constructor takes arguments --> VertexD3VOPointDepth(int u, int v) : _u(u), _v(v) {}
-    py::class_<VertexD3VOPointDepth, BaseVertex<1, double>>(m, "VertexD3VOPointDepth")
-        .def(py::init<const int, const int>())
-        .def("set_to_origin_impl", &VertexD3VOPointDepth::setToOriginImpl)
-        // .def("set_estimate", &VertexD3VOPointDepth::setEstimate)   // const VertexD3VOPointDepth& -> void
-        .def("oplus_impl", &VertexD3VOPointDepth::oplusImpl)
-        .def("set_estimate_data_impl", &VertexD3VOPointDepth::setEstimateDataImpl)
-        .def("get_estimate_data", &VertexD3VOPointDepth::getEstimateData)
-        .def("estimate_dimension", &VertexD3VOPointDepth::estimateDimension)
-        .def("set_minimal_estimate_data_impl", &VertexD3VOPointDepth::setMinimalEstimateDataImpl)
-        .def("get_minimal_estimate_data", &VertexD3VOPointDepth::getMinimalEstimateData)
-        .def("minimal_estimate_dimension", &VertexD3VOPointDepth::minimalEstimateDimension)
-    ;
 }
 
 }  // end namespace g2o
